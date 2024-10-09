181a3c6bcae4b8dca57c461c5276aace87b1771c
GustavaoF <gustavo.chaves.almeida@escola.pr.gov.br>
2024-10-09 08:43:38 -0300

    ;

---
+++ questions.js
@@ -0,0 +1,82 @@
+criaCartao(
+    'Física', 
+    'Qual a Equação Fundamental da Ondulatória?',
+    'V = lambda . F',
+
+    'Geografia', 
+    'Qual a capital do Espírito Santo?',
+    'Vitória',
+
+    'Matemática', 
+    'Zero é um numero par?',
+    'Não, ele é neutro',
+
+    'Geografia', 
+    'Qual a maior cidade do sul do Brasil?',
+    'Curitiba',
+
+    'Geografia', 
+    'Qual a capital de Portugal?',
+    'Lisboa',
+
+    'Geografia', 
+    'Quantos estados tem o Brasil?',
+    '26 estados mais o Distrito Federal',
+
+    'Matemática', 
+    'Qual a equação da combinatória de Arranjo?',
+    'Para An,p = n! / (n-p)!',
+
+    'Matemática', 
+    'Qual a Equação da combinatória de Combinação?',
+    'Para Cn,p = n!/ (n-p)! p!',
+
+    'Geografia', 
+    'Quantas cidades tem o Paraná?',
+    '399',
+
+    'Geografia', 
+    'Qual a cidade mais feia do Paraná?',
+    'Palmas',
+
+    'Geografia', 
+    'Quantos estados o Estados Unidos da América tem?',
+    '50',
+
+    'Biologia', 
+    'Qual parte das folhas de planta fazem a troca gasosa?',
+    'Estomato',
+
+    'Geografia', 
+    'Qual a capital da Itália?',
+    'Roma',
+
+    'Geografia', 
+    'Quantas regiões a Itália tem?',
+    '20',
+
+    'Geografia', 
+    'Qual a região mais alta da Itália?',
+    'Trentino Alto Ádige',
+
+    'Português', 
+    'Qual a data da última reforma ortográfica?',
+    '1 de Janeiro de 2009',
+
+    'História', 
+    'Qual a principal personagem da Reforma Protestante?',
+    'Martinho Lutero',
+
+    'História', 
+    'Quais são as três revoluções modernas da história?',
+    'Revolução Francesa, Industrial, e Reforma Protestante',
+    
+    'Química', 
+    'Hidrocarbonetos são solúveis em água?',
+    'não',
+
+    'Geografia', 
+    'Qual o menor país do mundo?',
+    'Vaticano',
+
+)
