65c2e6d8af9e06d0d16cc875eb861ece40016431
GustavaoF <gustavo.chaves.almeida@escola.pr.gov.br>
2024-10-09 08:45:25 -0300

    .

--- questions.js
+++ questions.js
@@ -2,79 +2,117 @@ criaCartao(
     'Física', 
     'Qual a Equação Fundamental da Ondulatória?',
     'V = lambda . F',
+)
 
+criaCartao(
     'Geografia', 
     'Qual a capital do Espírito Santo?',
     'Vitória',
+)
 
+criaCartao(
     'Matemática', 
     'Zero é um numero par?',
     'Não, ele é neutro',
+)
 
+criaCartao(
     'Geografia', 
     'Qual a maior cidade do sul do Brasil?',
     'Curitiba',
+)
 
+criaCartao(
     'Geografia', 
     'Qual a capital de Portugal?',
     'Lisboa',
+)
 
+criaCartao(
     'Geografia', 
     'Quantos estados tem o Brasil?',
     '26 estados mais o Distrito Federal',
+)
 
+criaCartao(
     'Matemática', 
     'Qual a equação da combinatória de Arranjo?',
     'Para An,p = n! / (n-p)!',
+)
 
+criaCartao(
     'Matemática', 
     'Qual a Equação da combinatória de Combinação?',
     'Para Cn,p = n!/ (n-p)! p!',
+)
 
+criaCartao(
     'Geografia', 
     'Quantas cidades tem o Paraná?',
     '399',
+)
 
+criaCartao(
     'Geografia', 
     'Qual a cidade mais feia do Paraná?',
     'Palmas',
+)
 
+criaCartao(
     'Geografia', 
     'Quantos estados o Estados Unidos da América tem?',
     '50',
+)
 
+criaCartao(
     'Biologia', 
     'Qual parte das folhas de planta fazem a troca gasosa?',
     'Estomato',
+)
 
+criaCartao(
     'Geografia', 
     'Qual a capital da Itália?',
     'Roma',
+)
 
+criaCartao(
     'Geografia', 
     'Quantas regiões a Itália tem?',
     '20',
+)
 
+criaCartao(
     'Geografia', 
     'Qual a região mais alta da Itália?',
     'Trentino Alto Ádige',
+)
 
+criaCartao(
     'Português', 
     'Qual a data da última reforma ortográfica?',
     '1 de Janeiro de 2009',
+)
 
+criaCartao(
     'História', 
     'Qual a principal personagem da Reforma Protestante?',
     'Martinho Lutero',
+)
 
+criaCartao(
     'História', 
     'Quais são as três revoluções modernas da história?',
     'Revolução Francesa, Industrial, e Reforma Protestante',
-    
+)
+
+criaCartao(
     'Química', 
     'Hidrocarbonetos são solúveis em água?',
     'não',
+)
 
+criaCartao(
     'Geografia', 
     'Qual o menor país do mundo?',
     'Vaticano',
